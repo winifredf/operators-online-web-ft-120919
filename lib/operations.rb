394ca852f6dae6dfc12532f57b61635a86ae8da4
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-17 02:51:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+  if  speed < 40 
+    return  "TRUE"
+  if speed > 60
+    return  "TRUE"
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed<40 ||>60 ? "TRUE" : "FALSE"
+	end
 end
 	
 
